ead53d5f498a8e3bce0088832fcaf7f1b69f7136
shewenbiao <shewenbiao@gmail.com>
2021-04-23 19:21:41 +0800

    在协程里启动JobService

--- library/src/main/java/com/keepalive/library/PermanentJobService.kt
+++ library/src/main/java/com/keepalive/library/PermanentJobService.kt
@@ -11,6 +11,8 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.core.content.PermissionChecker
 import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 /**
  * @author : She Wenbiao
@@ -27,40 +29,42 @@ class PermanentJobService : JobService() {
 
         fun start(context: Context, foregroundNotificationConfig: ForegroundNotificationConfig?) {
             this.foregroundNotificationConfig = foregroundNotificationConfig
-            val builder = JobInfo.Builder(
-                10, ComponentName(
-                    context.packageName,
-                    PermanentJobService::class.java.name
+            GlobalScope.launch {
+                val builder = JobInfo.Builder(
+                    10, ComponentName(
+                        context.packageName,
+                        PermanentJobService::class.java.name
+                    )
                 )
-            )
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                //7.0以上最小间隔是15分钟
-                builder.setPeriodic(ONE_HOUR)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    //7.0以上最小间隔是15分钟
+                    builder.setPeriodic(ONE_HOUR)
 //                setPeriodic：设置时间间隔，单位毫秒。该方法不能和
 //                setMinimumLatency、setOverrideDeadline这两个同时调用，
 //                否则会报错“java.lang.IllegalArgumentException:
 //                Can't call setMinimumLatency() on a periodic job”，
 //                或者报错“java.lang.IllegalArgumentException:
 //                Can't call setOverrideDeadline() on a periodic job”。
-            } else {
-                //每隔5分钟执行一次job
-                builder.setPeriodic(FIVE_MIN)
-            }
-            if (PermissionChecker.checkSelfPermission(
-                    context,
-                    Manifest.permission.RECEIVE_BOOT_COMPLETED
-                ) == PERMISSION_GRANTED
-            ) {
+                } else {
+                    //每隔5分钟执行一次job
+                    builder.setPeriodic(FIVE_MIN)
+                }
+                if (PermissionChecker.checkSelfPermission(
+                        context,
+                        Manifest.permission.RECEIVE_BOOT_COMPLETED
+                    ) == PERMISSION_GRANTED
+                ) {
 
 //                    setPersisted：重启后是否还要继续执行，此时需要声明权限RECEIVE_BOOT_COMPLETED，
 //                    否则会报错“java.lang.IllegalArgumentException:
 //                    Error: requested job be persisted without holding RECEIVE_BOOT_COMPLETED permission.”
 //                    而且RECEIVE_BOOT_COMPLETED需要在安装的时候就要声明，如果一开始没声明，
 //                    而在升级时才声明，那么依然会报权限不足的错误。
-                builder.setPersisted(true)
+                    builder.setPersisted(true)
+                }
+                val jobScheduler = context.getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
+                jobScheduler.schedule(builder.build())
             }
-            val jobScheduler = context.getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
-            jobScheduler.schedule(builder.build())
         }
     }
 
